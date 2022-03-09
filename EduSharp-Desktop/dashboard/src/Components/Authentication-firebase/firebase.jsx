9912486b4da48c13b2eafb7dbc9d3ac22d9e62bb
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-03-09 10:03:50 +0200

    firebase config

--- EduSharp-Desktop/dashboard/src/Components/Authentication-firebase/firebase.jsx
+++ EduSharp-Desktop/dashboard/src/Components/Authentication-firebase/firebase.jsx
@@ -2,17 +2,19 @@
 import firebase from 'firebase';
 import 'firebase/database'
 
-const Config = {
-  apiKey: "AIzaSyBWKSbPloiNsWtwSkB3HVwkM-mlTV0gwxE",
-  authDomain: "edusharp-auth.firebaseapp.com",
-  projectId: "edusharp-auth",
-  storageBucket: "edusharp-auth.appspot.com",
-  messagingSenderId: "697340654972",
-  appId: "1:697340654972:web:7d7621a7f6260a28e020f9",
-  measurementId: "G-4WXVFRH7L6"
+const firebaseConfig = {
+  apiKey: "AIzaSyDh51b15XlbShMIeETE3iroRxMpaPaUM5M",
+  authDomain: "edusharp-de629.firebaseapp.com",
+  databaseURL: "https://edusharp-de629-default-rtdb.firebaseio.com",
+  projectId: "edusharp-de629",
+  storageBucket: "edusharp-de629.appspot.com",
+  messagingSenderId: "257078871465",
+  appId: "1:257078871465:web:b99db41c817dc5f85ff0b8",
+  measurementId: "G-N8ZZCGZCG1"
 };
 
 
-firebase.initializeApp(Config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase.database()
+ 
--- EduSharp-Desktop/dashboard/src/Components/GetStarted/signUp.jsx
+++ EduSharp-Desktop/dashboard/src/Components/GetStarted/signUp.jsx
@@ -1,77 +1,92 @@
-import React,{useState} from 'react'
-import './style.css'
-import logo from '../images/image.png'
-import Users from '../Authentication-firebase/reuse'
+import React, { useState } from "react";
+import "./style.css";
+import logo from "../images/image.png";
+import Users from "../Authentication-firebase/reuse";
 const Register = () => {
-    const [firstname,setName] = useState()
-    const [lastname,setLastName] = useState()
-    const [email,setEmail] = useState()
-    const [password,setPassword] = useState()
+  const [firstname, setName] = useState();
+  const [lastname, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const SignUp =(e)=>{
-        e.preventDefault()
-        Users.signUp(email,password,firstname,lastname)
-        console.log('sent')
-    }
+  const SignUp = (e) => {
+    e.preventDefault();
+    Users.signUp(email, password, firstname, lastname);
+    console.log("sent");
+  };
   return (
-    <div className='ContainerRegister'>
-        <div className='backBody'>
-             <div className='head'>
-                <div className='logo'>
-                      <img src={logo} alt={'edusharp'} width={50} height={50}/>
-                <h3 color='white'>EduSharp</h3>
-                </div>
-                    <h3>Administration
-                        </h3>
-            </div>
-            <div className='signBody'>
-                <div className='headings'>
-                <h1>
-                    Create new account.
-                </h1>
-               
-                </div>
-  
-                <form className='Register' onSubmit={SignUp}>
-                    <div>
-                    <div className='input-icons'>
-                   {/* <i className='fa fa-id-card fa-2x'></i> */}
-                    <input type='text' placeholder='First name'
-                    className='input-field' value={firstname} onChange={(e)=>setName(e.target.value)} 
-                    />
-                   </div>
-                   <div className='input-icons'>
-                   {/* <i className='fa fa-id-card fa-2x'></i> */}
-                    <input type='text' placeholder='last name'
-                    className='input-field' value={lastname} onChange={(e)=>setLastName(e.target.value)} 
-                    />
-                   </div>
-                    
-                    </div>
-                    {/* other inputs */}
-                    <div className='input-icons'>
-                   {/* <i className='fa fa-envelope fa-2x'></i> */}
-                    <input type='email' placeholder='Email Address'
-                    className='input-field' value={email} onChange={(e)=>setEmail(e.target.value)} 
-                    />
-                   </div>
-                   <div className='input-icons'>
-                   {/* <i className='fa fa-eye fa-2x'></i> */}
-                    <input type='password' placeholder='Create Password'
-                    className='input-field' value={password} onChange={(e)=>setPassword(e.target.value)} 
-                    />
-                   </div>
-                   <div className='buttons'>
-                       {/* <button className='forgotButton'>Forgot Password</button> */}
-                       <button className='create' onClick={SignUp} type={'submit'}>Create Account</button>
-                   </div>
-                    <p>Already have an account? <a href='signIn'>Sign in</a></p>   
-                </form>
-            </div>
+    <div className="ContainerRegister">
+      <div className="backBody">
+        <div className="head">
+          <div className="logo">
+            <img src={logo} alt={"edusharp"} width={50} height={50} />
+            <h3 color="white">EduSharp</h3>
+          </div>
+          <h3>Administration</h3>
         </div>
-        
-    </div>
-  )
-}
+        <div className="signBody">
+          <div className="headings">
+            <h1>Create new account.</h1>
+          </div>
 
-export default Register
+          <form className="Register" onSubmit={SignUp}>
+            <div>
+              <div className="input-icons">
+                {/* <i className='fa fa-id-card fa-2x'></i> */}
+                <input
+                  type="text"
+                  placeholder="First name"
+                  className="input-field"
+                  value={firstname}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="input-icons">
+                {/* <i className='fa fa-id-card fa-2x'></i> */}
+                <input
+                  type="text"
+                  placeholder="last name"
+                  className="input-field"
+                  value={lastname}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+            </div>
+            {/* other inputs */}
+            <div className="input-icons">
+              {/* <i className='fa fa-envelope fa-2x'></i> */}
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="input-field"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="input-icons">
+              {/* <i className='fa fa-eye fa-2x'></i> */}
+              <input
+                type="password"
+                placeholder="Create Password"
+                className="input-field"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="buttons">
+              {/* <button className='forgotButton'>Forgot Password</button> */}
+              <button className="create" onClick={SignUp} type={"submit"}>
+                Create Account
+              </button>
+            </div>
+            <p>
+              Already have an account? <a href="signIn">Sign in</a>
+            </p>
+          </form>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Register;
