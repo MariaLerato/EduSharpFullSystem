92edc99353742046ccf6ccf79f05773a96c6591b
andries sebola <andriesmpontsheng@gmail.com>
2022-03-15 20:50:10 +0200

    commit

--- EduSharp/source/screens/Pro.jsx
+++ EduSharp/source/screens/Pro.jsx
@@ -8,12 +8,17 @@ import {
   Dimensions,
   ImageBackground
 } from "react-native";
-<<<<<<< HEAD
 // import Icon from 'react-native-vector-icons/MaterialIcons'
 import { Icon, Button } from "react-native-elements";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 // import * as ImagePicker from "expo-image-picker";
 
+
+const imagesize=100
+const imageradius=imagesize/2;
+const height = Dimensions.get("screen").height;
+const width = Dimensions.get("screen").width;
+const row1Height = height * 0.3;
 const Pro = ({ navigation }) => {
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -32,75 +37,20 @@ const Pro = ({ navigation }) => {
     // }
 
     // setSelectedImage({ localUri: pickerResult.uri });
-=======
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Checkbox, Snackbar, TextInput } from "react-native-paper";
-import { Icon, Input, Avatar } from "react-native-elements";
-import Styles from "../style/signinScreen";
-import { COLORS, SIZES, FONTS } from "../constants";
-import { db,auth } from "../BackendFirebase/configue/Firebase";
-
-import ProfileHome from "../components/ProfileHome";
-import EditProfile from "../components/EditProfile";
-import EducProfile from "../components/EducProfile";
-import { Button } from "react-native-elements/dist/buttons/Button";
-const imagesize=100
-const imageradius=imagesize/2
-const height = Dimensions.get("screen").height;
-const row1Height = height * 0.3;
-const row2Height = height * 0.7;
-const Stack = createNativeStackNavigator();
-
-const Pro = ({navigation}) => {
-  const [name, setName] = useState();
-  
-  const [email, setemail] = useState();
- const [url,seturl] = useState();
-  //
- 
-  const userId = auth.currentUser.uid;
-  const updateUser = () => {
-      db.ref('/users/' + userId).update({
-          name: name,
-          email: email,
-
-      });
   };
 
-//   useEffect(() => {
-//     db.ref('/users/' + userId).on('value', value => {
-//         console.log(value, 'value')
-//         setName(value?.val().name)
-        
-//         setemail(value.val().email)
-
-//     })
-// }, [])
-
-
-  const LocationArray = ["Home", "Profile", "Education"];
-  const [location, setLocation] = useState("Education");
-  const backgroundImg = {
-    uri: "https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  };
-  const backgroundImg2 = {
-    uri: "https://images.pexels.com/photos/3646172/pexels-photo-3646172.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
->>>>>>> 62b130e4ae074b2cd14e9dd94079ef032e378049
-  };
-
-  const profilimg={uri:'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'}
+  const profilimg = { uri: 'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
   return (
-<<<<<<< HEAD
     <>
       <ScrollView
         style={{ flex: 1, backgroundColor: "#ffffff" }}
         showsVerticalScrollIndicator={false}
       >
-        <ImageBackground
+        {/* <ImageBackground
           source={require("../../assets/images/Rectangle.jpg")}
           style={{ height: Dimensions.get("window").height / 2.9 }}
-        ></ImageBackground>
-        {/*Bottom View*/}
+        />
+        {/*Bottom View
 
         <View style={styles.bottomView}>
           <View style={styles.innerBottom}>
@@ -135,7 +85,7 @@ const Pro = ({navigation}) => {
                 />
               )}
               {/* <Image source={{ uri: 'https://image.shutterstock.com/image-vector/male-avatar-profile-picture-use-600w-193292033.jpg'}}
-                style={{height:120,width:120,borderRadius:60,}}/> */}
+                style={{height:120,width:120,borderRadius:60,}}/>
               <TouchableOpacity
                 style={{ marginLeft: 70, marginTop: -160 }}
                 mode="contained"
@@ -144,139 +94,127 @@ const Pro = ({navigation}) => {
                 <FontAwesome name="camera" size={29} color="grey" />
               </TouchableOpacity>
             </View>
-=======
-    <View style={styles.container}>
-      <ImageBackground source={backgroundImg} resizeMode="cover" style={styles.row1}></ImageBackground>
-      <View style={styles.row2}>
-        <View style={styles.imgContainer}>
-            <Avatar      source={profilimg}  style={styles.image} rounded/>
-            <Button   icon={{ name: "camera", type: "font-awesome", size: 15, color: "black" }} containerStyle={{position:'absolute',bottom:-10,right:0}}/>
-        </View>
-       
-        </View>
-       <View style={styles.elevated}>
-        <TouchableOpacity onPress={()=>navigation.navigate('profile')} >
->>>>>>> 62b130e4ae074b2cd14e9dd94079ef032e378049
             <View style={styles.boxcontainer}>
-            <Icon name="person" size={22} style={{ color: COLORS.secondary, marginTop: 14,  }} />
+              <Icon name="person" size={22} style={{ color: COLORS.secondary, marginTop: 14, }} />
 
               <Text style={styles.boxText}>Personal Information</Text>
               <Icon name="arrow-forward-ios" size={20} style={{ color: COLORS.gray, marginTop: 14, paddingLeft: 90 }} />
 
             </View>
 
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=>navigation.navigate('education')} >
-            <View style={styles.boxcontainer}>
-            <Icon name="book" size={22} style={{ color: COLORS.secondary, marginTop: 14,  }} />
+            <TouchableOpacity onPress={() => navigation.navigate('education')} >
+              <View style={styles.boxcontainer}>
+                <Icon name="book" size={22} style={{ color: COLORS.secondary, marginTop: 14, }} />
 
-              <Text style={styles.boxText}>Educational Information</Text>
-              <Icon name="arrow-forward-ios" size={20} style={{ color: COLORS.gray, marginTop: 14, paddingLeft: 70 }} />
+                <Text style={styles.boxText}>Educational Information</Text>
+                <Icon name="arrow-forward-ios" size={20} style={{ color: COLORS.gray, marginTop: 14, paddingLeft: 70 }} />
 
-            </View>
-
-            </TouchableOpacity>
               </View>
-      
-    </View>
-  );
+
+            </TouchableOpacity>
+          </View>
+
+        </View> */}
+        </ScrollView>
+      </>
+      );
 };
-const { width} = Dimensions.get("screen");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'white'
-  },
-  row1: {
-    height: row1Height,
-  },
-  row2: {
-    backgroundColor: "white",
-    flex: 1,
-    marginTop: -80,
-    borderTopRightRadius: 30,
-    borderTopLeftRadius: 30,
-    alignItems: "center",
-  },
-  imgContainer: {
-    width: imagesize,
-    height: imagesize,
-    marginTop: -50,
-    borderRadius: imageradius,
-    borderWidth: 2,
-    borderColor: "white",
-    overflow:'hidden'
-    
-  },
-  usernames: {
-    alignItems: "center",
-  },
-  image:{
-      width:'100%',
-      height:'100%'
-  },
-  innerBottom: {
-    paddingHorizontal: 15,
-    top: 50,
-  },
 
-  bottomView: {
-    flex: 1.5,
-    backgroundColor: "#fff",
-    bottom: 40,
-    borderTopStartRadius: 40,
-    borderTopEndRadius: 40,
+      const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+      backgroundColor: 'white'
   },
-  elevated:{
-paddingHorizontal:20,
-top:-280
+      row1: {
+        height: row1Height,
   },
-  boxcontainer: {
-    width: width / 1.1,
-    height: height / 12.3,
-    elevation: 15,
-    borderRadius: 10,
-    backgroundColor: "#fff",
-    marginTop: 20,
-    paddingHorizontal: 20,
-    flexDirection: "row",
+      row2: {
+        backgroundColor: "white",
+      flex: 1,
+      marginTop: -80,
+      borderTopRightRadius: 30,
+      borderTopLeftRadius: 30,
+      alignItems: "center",
   },
-  logOut: {
-    paddingTop: 130,
+      imgContainer: {
+        width: imagesize,
+      height: imagesize,
+      marginTop: -50,
+      borderRadius: imageradius,
+      borderWidth: 2,
+      borderColor: "white",
+      overflow: 'hidden'
+
   },
-  containerStyle: {
-    width: 300,
-    marginVertical: 10,
-    borderColor: "gray",
-    elevation: 12,
+      usernames: {
+        alignItems: "center",
   },
-  buttonStyle: {
-    backgroundColor: "white",
-    borderColor: "transparent",
-    borderWidth: 0,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "rgba(0,0,0,.2)",
+      image: {
+        width: '100%',
+      height: '100%'
+  },
+      innerBottom: {
+        paddingHorizontal: 15,
+      top: 50,
   },
 
-  boxText: {
-    fontSize: 16,
-    marginTop: 15,
+      bottomView: {
+        flex: 1.5,
+      backgroundColor: "#fff",
+      bottom: 40,
+      borderTopStartRadius: 40,
+      borderTopEndRadius: 40,
+  },
+      elevated: {
+        paddingHorizontal: 20,
+      top: -280
+  },
+      boxcontainer: {
+        width: width / 1.1,
+      height: height / 12.3,
+      elevation: 15,
+      borderRadius: 10,
+      backgroundColor: "#fff",
+      marginTop: 20,
+      paddingHorizontal: 20,
+      flexDirection: "row",
+  },
+      logOut: {
+        paddingTop: 130,
+  },
+      containerStyle: {
+        width: 300,
+      marginVertical: 10,
+      borderColor: "gray",
+      elevation: 12,
+  },
+      buttonStyle: {
+        backgroundColor: "white",
+      borderColor: "transparent",
+      borderWidth: 0,
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: "rgba(0,0,0,.2)",
+  },
+
+      boxText: {
+        fontSize: 16,
+      marginTop: 15,
   },
 });
-export default Pro;
+      export default Pro;
 
-{
-  /* <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="HotelsView" component={HotelsView} /> 
-      <Stack.Screen name="Search" component={Search} />
-    </Stack.Navigator> */
-}
+      {
+        /* <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="HotelsView" component={HotelsView} /> 
+            <Stack.Screen name="Search" component={Search} />
+          </Stack.Navigator> */
+      }
 
 
